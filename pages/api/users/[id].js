423ccc6f7d71d32f34dbab01fdb9add0dbdd5611
Sebastian Scherbes <github@scherbes.de>
2022-10-09 12:12:03 +0200

    solve all challenges

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,3 +1,25 @@
+import {getUserById, deleteUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
+  if (request.method === 'GET') {
+    try {
+      const data = await getUserById(request.query.id);
+      return response.status(200).json(data);
+    } catch (err) {
+      return response.status(404).json({message: 'Error: user not found.'});
+    }
+  }
+
+  if (request.method === 'DELETE') {
+    try {
+      const deletedUser = await deleteUser(request.query.id);
+      return response
+        .status(200)
+        .json({message: 'Info: user deleted successfully.'});
+    } catch (err) {
+      return response.status(404).json({message: 'Error: user not found.'});
+    }
+  }
+
   res.status(403).json({message: 'Error: request method not allowed.'});
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,7 +1,25 @@
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+} from '../../../services/userService';
+
 export default async function handler(request, response) {
   if (request.method === 'GET') {
-    const data = [];
+    const data = await getAllUsers();
     return response.status(200).json(data);
+  }
+
+  if (request.method === 'POST') {
+    const data = JSON.parse(request.body);
+    try {
+      const newUser = await createUser(data.name, data.gender, data.email);
+      return response.status(201).json(newUser);
+    } catch (err) {
+      return response
+        .status(400)
+        .json({message: 'Error: Can not create user.'});
+    }
   }
 
   res.status(403).json({message: 'Error: request method not allowed.'});
